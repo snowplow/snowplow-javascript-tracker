d823e6640e796ecb11d7eefc3d9e168c8e20d1a8
Alex Dean <alex@snowplowanalytics.com>
2014-01-18 21:07:51 +0000

    Updated copyright notices

--- packages/core/src/js/helpers.js
+++ packages/core/src/js/helpers.js
@@ -2,7 +2,7 @@
  * JavaScript tracker for Snowplow: helpers.js
  * 
  * Significant portions copyright 2010 Anthon Pang. Remainder copyright 
- * 2012-2013 Snowplow Analytics Ltd. All rights reserved. 
+ * 2012-2014 Snowplow Analytics Ltd. All rights reserved. 
  * 
  * Redistribution and use in source and binary forms, with or without 
  * modification, are permitted provided that the following conditions are 
