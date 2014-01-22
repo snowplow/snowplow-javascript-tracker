066bec3abb8d892d15464ee6158b9d19c870b591
Alex Dean <alexander.dean@keplarllp.com>
2014-01-22 23:27:39 +0000

    Added base64decode to Tracker (#36)

--- src/js/lib/base64.js
+++ src/js/lib/base64.js
@@ -65,3 +65,45 @@ SnowPlow.base64urlencode = function(data) {
             .replace(/\+/g, '-')
             .replace(/\//g, '_');
 };
+
+/*
+ * Base64 decode data
+ */
+SnowPlow.base64decode = function(data) {
+  var keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+  var output = "";
+  var chr1, chr2, chr3 = "";
+  var enc1, enc2, enc3, enc4 = "";
+  var i = 0;
+
+  // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
+  var base64test = /[^A-Za-z0-9\+\/\=]/g;
+
+  data = data.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+  do {
+    enc1 = keyStr.indexOf(data.charAt(i++));
+    enc2 = keyStr.indexOf(data.charAt(i++));
+    enc3 = keyStr.indexOf(data.charAt(i++));
+    enc4 = keyStr.indexOf(data.charAt(i++));
+
+    chr1 = (enc1 << 2) | (enc2 >> 4);
+    chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+    chr3 = ((enc3 & 3) << 6) | enc4;
+
+    output = output + String.fromCharCode(chr1);
+
+    if (enc3 != 64) {
+      output = output + String.fromCharCode(chr2);
+    }
+
+    if (enc4 != 64) {
+      output = output + String.fromCharCode(chr3);
+    }
+
+    chr1 = chr2 = chr3 = "";
+    enc1 = enc2 = enc3 = enc4 = "";
+  } while (i < data.length);
+
+  return unescape(output);
+};
