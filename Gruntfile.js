62dce55405fbde3c1e5354107a9283dec0719888
Fred Blundun <fred@snowplowanalytics.com>
2014-02-10 16:14:16 +0000

    Moved part of banner.js into Gruntfile with grunt-concat's banner option so its values are based on package.json (#82)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,36 +1,47 @@
 /*
- * JavaScript tracker for Snowplow: Gruntfile.js
- * 
- * Significant portions copyright 2010 Anthon Pang. Remainder copyright 
- * 2012-2014 Snowplow Analytics Ltd. All rights reserved. 
- * 
- * Redistribution and use in source and binary forms, with or without 
- * modification, are permitted provided that the following conditions are 
- * met: 
- *
- * * Redistributions of source code must retain the above copyright 
- *   notice, this list of conditions and the following disclaimer. 
- *
- * * Redistributions in binary form must reproduce the above copyright 
- *   notice, this list of conditions and the following disclaimer in the 
- *   documentation and/or other materials provided with the distribution. 
- *
- * * Neither the name of Anthon Pang nor Snowplow Analytics Ltd nor the
- *   names of their contributors may be used to endorse or promote products
- *   derived from this software without specific prior written permission. 
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR 
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+* JavaScript tracker for Snowplow: Gruntfile.js
+*
+* Significant portions copyright 2010 Anthon Pang. Remainder copyright
+* 2012-2014 Snowplow Analytics Ltd. All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are
+* met:
+*
+* * Redistributions of source code must retain the above copyright
+* notice, this list of conditions and the following disclaimer.
+*
+* * Redistributions in binary form must reproduce the above copyright
+* notice, this list of conditions and the following disclaimer in the
+* documentation and/or other materials provided with the distribution.
+*
+* * Neither the name of Anthon Pang nor Snowplow Analytics Ltd nor the
+* names of their contributors may be used to endorse or promote products
+* derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+* A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+* OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+* SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+* LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+* DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+* THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+var semver = require('semver');
+var banner = "/*!" +
+" * Snowplow - The world's most powerful web analytics platform\n" +
+" *\n" +
+" * @description JavaScript tracker for Snowplow\n" +
+" * @version     --version--\n" +
+" * @author      --authors--\n" +
+" * @copyright   Anthon Pang, Snowplow Analytics Ltd\n" +
+" * @license     --license--\n" +
+" */\n\n"
 
 var semver = require('semver');
 
@@ -41,7 +52,16 @@ module.exports = function(grunt) {
   var semVer = semver.parse(pkg.version);
   pkg.pinnedVersion = semVer.major;
 
+  console.log(pkg.version);
+  banner = banner.replace('--version--', pkg.version);
+  banner = banner.replace('--authors--', pkg.contributors.join(', '));
+  banner = banner.replace('--license--', pkg.license);
+
+
+
   grunt.initConfig({
+
+    banner: banner,
 
     pkg: pkg,
 
@@ -50,7 +70,8 @@ module.exports = function(grunt) {
     concat: {
       dist: {
         options: {
-          'report': 'gzip'
+          'report': 'gzip',
+          'banner': '<%= banner %>'
         },
         src: ['src/js/banner.js',
               'src/js/lib/json.js',
@@ -76,7 +97,7 @@ module.exports = function(grunt) {
         options: {
           linebreak: 1000,
           report: 'gzip'
-        },  
+        },
         files: [
           {
           src: 'dist/snowplow.js',
@@ -99,7 +120,7 @@ module.exports = function(grunt) {
           {
             src: 'dist/sp.js',
             dest: '<%= pkg.version %>/sp.js'
-          }      
+          }
         ]
       },
       pinned: {
