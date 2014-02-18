c8328df03edaac1627b7d74538cb4a2eb6244047
Fred Blundun <fred@snowplowanalytics.com>
2014-02-18 17:49:01 +0000

    Turned test suite into two separate tests

--- tests/queueT.js
+++ tests/queueT.js
@@ -3,30 +3,32 @@ define([
 	'intern/chai!assert',
 	'src/js/queue.js',
 ], function(registerSuite, assert, queue) {
+
+	var MockTracker = function () {
+		var attribute = 10;
+		return {
+			increase: function(n) {
+				attribute += n;
+			},
+			getAttribute: function() {
+				return attribute;
+			}
+		}
+	};
+
+	var mockTracker = new MockTracker();
+	var snaq = [['increase', 5]];
+	snaq = new AsyncQueueProxy(mockTracker, snaq);
+
 	registerSuite({
 		name: 'queue test',
 		'make a proxy': function() {
+			assert.equal(mockTracker.getAttribute(), 15, 'Function originally stored in snaq gets executed when snaq becomes an AsyncQueueProxy');
+		},
 
-			var MockTracker = function () {
-				var attribute = 10;
-				return {
-					increase: function(n) {
-						attribute += n;
-					},
-					getAttribute: function() {
-						return attribute;
-					}
-				}
-			};
-
-			var mockTracker = new MockTracker();
-
-			var snaq = [['increase', 5]];
-			snaq = new AsyncQueueProxy(mockTracker, snaq);
-			assert.equal(mockTracker.getAttribute(), 15, 'The function originally stored in snaq gets executed');
-			
+		'add to snaq after conversion': function() {
 			snaq.push(['increase', 7]);
-			assert.equal(mockTracker.getAttribute(), 22, 'The function added to snaq after it becomes a proxy gets executed');
+			assert.equal(mockTracker.getAttribute(), 22, 'Function added to snaq after it becomes an AsyncQueueProxy gets executed')
 		}
 	});
 });
