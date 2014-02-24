86d14af9ac381c38de4c1c5889bbec92747a3f6d
Fred Blundun <fred@snowplowanalytics.com>
2014-02-24 13:59:30 +0000

    Renamed test files. Added copyright notices. Edited CHANGELOG.

--- tests/queue.js
+++ tests/queue.js
@@ -1,5 +1,5 @@
 /*
- * JavaScript tracker for Snowplow: tests/intern.js
+ * JavaScript tracker for Snowplow: tests/queue.js
  * 
  * Significant portions copyright 2010 Anthon Pang. Remainder copyright 
  * 2012-2014 Snowplow Analytics Ltd. All rights reserved. 
@@ -32,9 +32,40 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-define({
+define([
+	'intern!object',
+	'intern/chai!assert',
+	'intern/dojo/node!../src/js/queue'
+], function(registerSuite, assert, queue) {
 
-// Non-functional test suites
-suites: ['tests/queueT', 'tests/payloadT'],
+	var MockTracker = function () {
+		var attribute = 10;
+		return {
+			increaseAttribute: function(n) {
+				attribute += n;
+			},
+			setAttribute: function(p) {
+				attribute = p;
+			},
+			getAttribute: function() {
+				return attribute;
+			}
+		}
+	};
 
+	var mockTracker = MockTracker(),
+		snaq = [['increaseAttribute', 5]];
+	snaq = new queue.AsyncQueueProxy(mockTracker, snaq);
+
+	registerSuite({
+		name: 'queue test',
+		'make a proxy': function() {
+			assert.equal(mockTracker.getAttribute(), 15, 'Function originally stored in snaq is executed when snaq becomes an AsyncQueueProxy');
+		},
+
+		'add to snaq after conversion': function() {
+			snaq.push(['setAttribute', 7]);
+			assert.equal(mockTracker.getAttribute(), 7, 'Function added to snaq after it becomes an AsyncQueueProxy is executed');
+		}
+	});
 });
