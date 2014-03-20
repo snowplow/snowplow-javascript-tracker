c63e1493bb1e4a9e8cdb86e3a897088de3d98e57
Fred Blundun <fred@snowplowanalytics.com>
2014-03-20 11:08:46 +0000

    Switched to more rigorous IP address validation regexp

--- src/js/lib/proxies.js
+++ src/js/lib/proxies.js
@@ -44,17 +44,26 @@
 		};
 
 	/*
-	 * If the hostname is of the 
+	 * Test whether a string is an IP address
 	 */
-	function isYahooCachedPage(hostname) {
-		var IPRegExp = new RegExp('^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$'),
+	function isIpAddress(string) {
+		var IPRegExp = new RegExp('^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$');
+		return IPRegExp.test(string);
+	}
+
+	/*
+	 * If the hostname is an IP address, look for text indicating
+	 * that the page is cached by Yahoo
+	 */
+	function isYahooCachedPage(hostName) {
+		var
 			initialDivText,
-			cachedText;
-		if (IPRegExp.test(hostname)) {
+			cachedIndicator;
+		if (isIpAddress(hostName)) {
 			try {
 				initialDivText = document.body.children[0].children[0].children[0].children[0].children[0].children[0].innerHTML;
-				cachedText = 'You have reached the cached page for';
-				return initialDivText.slice(0, cachedText.length) === cachedText; 
+				cachedIndicator = 'You have reached the cached page for';
+				return initialDivText.slice(0, cachedIndicator.length) === cachedIndicator; 
 			} catch (e) {
 				return false;
 			};
@@ -62,22 +71,22 @@
 	}
 
 	/*
+	 * Extract parameter from URL
+	 */
+	function getParameter(url, name) {
+		// scheme : // [username [: password] @] hostname [: port] [/ [path] [? query] [# fragment]]
+		var e = new RegExp('^(?:https?|ftp)(?::/*(?:[^?]+))([?][^#]+)'),
+			matches = e.exec(url),
+			result = helpers.fromQuerystring(name, matches[1]);
+
+		return result;
+	}
+
+	/*
 	 * Fix-up URL when page rendered from search engine cache or translated page.
 	 * TODO: it would be nice to generalise this and/or move into the ETL phase.
 	 */
 	object.fixupUrl = function (hostName, href, referrer) {
-
-		/*
-		 * Extract parameter from URL
-		 */
-		function getParameter(url, name) {
-			// scheme : // [username [: password] @] hostname [: port] [/ [path] [? query] [# fragment]]
-			var e = new RegExp('^(?:https?|ftp)(?::/*(?:[^?]+))([?][^#]+)'),
-				matches = e.exec(url),
-				result = helpers.fromQuerystring(name, matches[1]);
-
-			return result;
-		}
 
 		if (hostName === 'translate.googleusercontent.com') {       // Google
 			if (referrer === '') {
