steve-gh/plow-fork

Bad requests in outQueue now no longer block good requests

Gruntfile.js
@@ -81,7 +81,7 @@ module.exports = function(grunt) {
       build: {
         dest: 'src/js/lib_managed/lodash.js',
         options: {
-          include: 'isArray, isFunction, isString, isObject, isDate, isUndefined, isNull, values',
+          include: 'isArray, isFunction, isString, isObject, isDate, isUndefined, isNull, values, compact',
           flags: ['debug']
         }
       }

snowplow/snowplow-javascript-tracker

Gruntfile.js
@@ -82,7 +82,7 @@ module.exports = function(grunt) {
         dest: 'src/js/lib_managed/lodash.js',
         options: {
           exports: 'node',
-          include: 'isArray, isFunction, isString, isObject, isDate, isUndefined, isNull, map',
+          include: 'isArray, isFunction, isString, isObject, isDate, isUndefined, isNull, map, compact',
           flags: ['debug']
         }
       }