6f02fb08ff82c44f58796e68718415c258433462
Alex Dean <alex@snowplowanalytics.com>
2014-11-15 13:28:19 +0000

    Merge branch 'release/2.1.2'

--- packages/core/tests/functional/detectors.js
+++ packages/core/tests/functional/detectors.js
@@ -41,7 +41,7 @@ define([
 	var 
 		// Expected viewport dimensions vary based on browser
 		expectedViewportWidths = [980, 1016, 1024, 1280],
-		expectedViewportHeights = [636, 644, 667, 670, 673, 684, 686, 695, 702, 705, 706, 707, 712],
+		expectedViewportHeights = [636, 642, 644, 667, 670, 673, 684, 686, 695, 702, 705, 706, 707, 712],
 
 		// User fingerprint varies based on browser features
 		// TODO: try to hash this off the useragent - 
@@ -52,10 +52,10 @@ define([
 			3875211103, // IE11
 			1627396823, // Firefox 27.0 Mac
 			972065378,  // Firefox 27.0 XP
-			2245575966, // Firefox 27.0 Linux
+			3434304937, // Firefox 27.0 Linux
 			3832438128, // Chrome 32.0 Mac OS X
 			2660492875, // Chrome 32.0 Windows NT
-			2938153066, // Chrome 32.0 Linux
+			3477630609, // Chrome 32.0 Linux
 			2504300905, // Safari 7.0 Mac
 			1891541158  // Safari 6.0.5 Mac
 		];
