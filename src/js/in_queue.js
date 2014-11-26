1879c3c1a98326e48c9fe7f0b0cc59e9d6bfb7bd
Rob Murphy <robm@metail.co.uk>
2014-11-26 15:31:15 +0000

    Use tracker as context for onload callbacks, call once per tracker

--- src/js/in_queue.js
+++ src/js/in_queue.js
@@ -150,9 +150,12 @@
 				// Arguments is not an array, so we turn it into one
 				input = Array.prototype.shift.call(parameterArray);
 
-				// Custom callback rather than tracker method
+				// Custom callback rather than tracker method, called for each named tracker
 				if (lodash.isFunction(input)) {
-					input.apply(this, parameterArray);
+					var allNamedTrackers = getNamedTrackers()
+					for (j = 0; j < allNamedTrackers.length; j++) {
+						input.apply(allNamedTrackers[j], parameterArray);
+					}
 					continue;
 				}
 
