7ad07ef255d516d8001f5f315c6772a50801152c
Anton Parkhomenko <mailbox@chuwy.me>
2016-10-26 23:14:15 +0700

    Get Code Climate badge to green (close #152)

--- tests/integration/integration.js
+++ tests/integration/integration.js
@@ -2,7 +2,7 @@
  * JavaScript tracker for Snowplow: tests/functional/helpers.js
  *
  * Significant portions copyright 2010 Anthon Pang. Remainder copyright
- * 2012-2014 Snowplow Analytics Ltd. All rights reserved.
+ * 2012-2016 Snowplow Analytics Ltd. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
