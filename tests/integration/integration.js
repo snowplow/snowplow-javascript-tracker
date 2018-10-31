51becbced6230385b956e4a35a75c69a3e4f6d03
mhadam <michael@hadam.us>
2018-10-31 15:24:37 -0400

    fixing tests

--- tests/integration/integration.js
+++ tests/integration/integration.js
@@ -220,13 +220,13 @@ define([
 						lodash.filter(contexts,
 							lodash.overSome(
 								lodash.matches({
-									schema: "iglu:/com.snowplowanalytics.snowplow/mobile_context/jsonschema/1-0-1",
+									schema: "iglu:com.snowplowanalytics.snowplow/mobile_context/jsonschema/1-0-1",
 									data: {
 										osType: 'ubuntu'
 									}
 								}),
 								lodash.matches({
-									schema: 'iglu:/com.snowplowanalytics.snowplow/geolocation_context/jsonschema/1-1-0',
+									schema: 'iglu:com.snowplowanalytics.snowplow/geolocation_context/jsonschema/1-1-0',
 									data: {
 										'latitude': '40.0',
 										'longitude': '55.1'
