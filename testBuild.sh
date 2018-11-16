bfab3632a33444e1badab1e6e8a22529f31fab30
mhadam <michael@hadam.us>
2018-11-16 16:48:17 -0500

    Finished testing workflow update

--- testBuild.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd core
-grunt
-cd ..
-grunt
-cp ./dist/snowplow.js ~/Dropbox/Tracker\ testing/JS/
-grunt concat:local
-cp ./tests/pages/integration-local.html ~/Dropbox/Tracker\ testing/JS/integration.html
