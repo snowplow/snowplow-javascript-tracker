d7319f75fb0188dab163a0f501b9cdd8af9a4bcc
Paul Boocock <paul@snowplowanalytics.com>
2020-01-10 17:26:50 +0000

    Reset activity on page view (close #750)

--- tests/unit/tracker.spec.js
+++ tests/unit/tracker.spec.js
@@ -86,6 +86,7 @@ describe('Activity tracker behaviour', () => {
       '',
       { outQueues },
       {
+        resetActivityTrackingOnPageView: false,
         stateStorageStrategy: 'cookies',
         encodeBase64: false,
         contexts: {
@@ -164,7 +165,47 @@ describe('Activity tracker behaviour', () => {
     expect(countWithStaticValueEq(pageTwoTime)(outQueues)).toBe(0)
   })
 
-  it('does not reset activity tracking on pageview', () => {
+  it('does not reset activity tracking on pageview when resetActivityTrackingOnPageView: false,', () => {
+    const outQueues = []
+    const Tracker = require('../../src/js/tracker').Tracker
+    const t = new Tracker(
+      '',
+      '',
+      '',
+      { outQueues },
+      {
+        resetActivityTrackingOnPageView: false,
+        stateStorageStrategy: 'cookies',
+        encodeBase64: false,
+        contexts: {
+          webPage: true,
+        },
+      }
+    )
+    t.enableActivityTracking(0, 30)
+    t.trackPageView()
+
+    advanceBy(15000)
+    jest.advanceTimersByTime(15000)
+
+    // activity on page one
+    t.updatePageActivity()
+    advanceBy(1000)
+
+    // shift to page two and trigger tick
+    t.trackPageView()
+    advanceBy(14000)
+    jest.advanceTimersByTime(15000)
+
+    // Activity was triggered on the first page.
+    // Activity tracking is currently not reset per page view so it is reported as happening on the second page.
+
+    const pps = getPPEvents(outQueues)
+
+    expect(F.size(pps)).toBe(1)
+  })
+
+  it('does reset activity tracking on pageview by default', () => {
     const outQueues = []
     const Tracker = require('../../src/js/tracker').Tracker
     const t = new Tracker(
@@ -195,17 +236,83 @@ describe('Activity tracker behaviour', () => {
     advanceBy(14000)
     jest.advanceTimersByTime(15000)
 
-    // Activity was triggered on the first page.
-    // Activity tracking is currently not reset per page view so it is reported as happening on the second page.
+    // Activity began tracking on the first page but moved on before 30 seconds.
+    // Activity tracking should still not have fire despite being on site 30 seconds, as user has moved page.
 
     const pps = getPPEvents(outQueues)
 
-    expect(F.size(pps)).toBe(1)
+    expect(F.size(pps)).toBe(0)
+  })
 
-    const pp = F.head(pps)
+  it('fires initial delayed activity tracking on first pageview and second pageview', () => {
+    const outQueues = []
+    const Tracker = require('../../src/js/tracker').Tracker
+    const t = new Tracker(
+      '',
+      '',
+      '',
+      { outQueues },
+      {
+        stateStorageStrategy: 'cookies',
+        encodeBase64: false,
+        contexts: {
+          webPage: true,
+        },
+      }
+    )
+    t.enableActivityTracking(10, 5)
 
+    t.trackPageView()
+    const firstPageId = t.getPageViewId()
+    advanceBy(5000)
+    jest.advanceTimersByTime(5000)
+
+    // callback timer starts tracking
+    advanceBy(1000)
+    t.updatePageActivity()
+    advanceBy(4000)
+    jest.advanceTimersByTime(5000)
+
+    // page ping timer starts tracking
+    advanceBy(1000)
+    t.updatePageActivity()
+    advanceBy(4000)
+    jest.advanceTimersByTime(5000)
+
+    advanceBy(1000)
+    t.updatePageActivity()
+    advanceBy(4000)
+    jest.advanceTimersByTime(5000)
+
+    t.trackPageView()
     const secondPageId = t.getPageViewId()
+    advanceBy(5000)
+    jest.advanceTimersByTime(5000)
 
-    expect(secondPageId).toBe(extractPageId(pp))
+    // window for callbacks ticks
+    advanceBy(1000)
+    t.updatePageActivity()
+    advanceBy(4000)
+    jest.advanceTimersByTime(5000)
+
+    // window for page ping ticks
+    advanceBy(1000)
+    t.updatePageActivity()
+    advanceBy(4000)
+    jest.advanceTimersByTime(5000)
+
+    // Activity began tracking on the first page and tracked two page pings in 16 seconds.
+    // Activity tracking only fires one further event over next 11 seconds as a page view event occurs, resetting timer back to 10 seconds.
+
+    const pps = getPPEvents(outQueues)
+
+    expect(F.size(pps)).toBe(3)
+
+    const pph = F.head(pps)
+    const ppl = F.last(pps)
+
+    expect(firstPageId).toBe(extractPageId(pph))
+    expect(secondPageId).toBe(extractPageId(ppl))
+
   })
 })
