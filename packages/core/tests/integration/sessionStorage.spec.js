3ebf16bcee2b68586a437ed5934f48e834dd19fd
Paul Boocock <paul@snowplowanalytics.com>
2020-08-17 09:15:22 +0100

    Update Copyright notices to 2020 (close #826)

--- packages/core/tests/integration/sessionStorage.spec.js
+++ packages/core/tests/integration/sessionStorage.spec.js
@@ -2,7 +2,7 @@
  * JavaScript tracker for Snowplow: tests/functional/integration.spec.js
  *
  * Significant portions copyright 2010 Anthon Pang. Remainder copyright
- * 2012-2016 Snowplow Analytics Ltd. All rights reserved.
+ * 2012-2020 Snowplow Analytics Ltd. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
