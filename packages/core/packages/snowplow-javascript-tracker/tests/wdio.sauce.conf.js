005f52bd3868d496cb9c39e394079e8d27d3f9b5
Paul Boocock <paul@snowplowanalytics.com>
2020-12-01 15:46:25 +0000

    Upgrade WebDriverIO to 6.10 (close #864)

--- packages/core/packages/snowplow-javascript-tracker/tests/wdio.sauce.conf.js
+++ packages/core/packages/snowplow-javascript-tracker/tests/wdio.sauce.conf.js
@@ -127,7 +127,16 @@ exports.config = {
       build: buildName,
     },
   ],
-  services: ['static-server', 'sauce'],
-  sauceConnect: true,
-  sauceConnectOpts: {},
+  services: [
+    ['sauce', {
+      sauceConnect: true,
+      sauceConnectOpts: {}
+    }], 
+    ['static-server', {
+      folders: [
+        { mount: '/', path: './tests/pages' }
+      ],
+      port: 8080
+    }]
+  ]
 }
