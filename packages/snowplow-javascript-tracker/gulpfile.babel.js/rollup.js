f89785172cce979d94001ba3b761f4109b775344
Paul Boocock <paul@snowplowanalytics.com>
2020-12-09 20:06:20 +0000

    Add cjs and esm builds

--- packages/snowplow-javascript-tracker/gulpfile.babel.js/rollup.js
+++ packages/snowplow-javascript-tracker/gulpfile.babel.js/rollup.js
@@ -1,10 +1,24 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
 import { babel } from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import banner from 'rollup-plugin-banner'
+import compiler from '@ampproject/rollup-plugin-closure-compiler';
+import cleanup from 'rollup-plugin-cleanup';
+import sizes from 'rollup-plugin-sizes';
+import filesize from 'rollup-plugin-filesize';
 
-export const basePlugins = [
+export const sizeReport = [
+    sizes(), 
+    filesize({showMinifiedSize: false, showBeforeSizes: 'build'})
+]
+
+export const minify = [
+    compiler(), 
+    cleanup({comments: 'none'})
+]
+
+export const core = [
     json(),
     nodeResolve({
         browser: true,
@@ -31,11 +45,11 @@ export const basePlugins = [
     })
 ];
 
-const licenseBanner = 
+const bannerContent = 
 "@description <%= pkg.description %>\n" +
 "@version     <%= pkg.version %>\n" +
 "@copyright   Anthon Pang, Snowplow Analytics Ltd\n" +
 "@license     <%= pkg.license %>\n\n" +
 "Documentation: http://bit.ly/sp-js";
 
-export const spBannerPlugin = banner(licenseBanner);
+export const licenseBanner = banner(bannerContent);
