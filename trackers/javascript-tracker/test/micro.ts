95c0ae44c91cdefe88f7ec4afbdf2180999f181a
Greg Leonard <gregjamesleonard@gmail.com>
2021-10-14 18:37:21 +0100

    add fetchMostRecentResult

--- trackers/javascript-tracker/test/micro.ts
+++ trackers/javascript-tracker/test/micro.ts
@@ -113,3 +113,7 @@ const createMicroCall = (url: string) => () =>
 
 export const fetchResults = (containerUrl: string) =>
   createMicroCall(`http://${containerUrl}/micro/good`)().then((good: unknown) => JSON.parse(good as string));
+
+export const fetchMostRecentResult = (containerUrl: string) => {
+  return createMicroCall(`http://${containerUrl}/micro/good`)().then((good: unknown) => JSON.parse(good as string)[0]);
+};
