bc48f0ffac34929323fa108e9fca16aae4b907ab
Matúš Tomlein <matus.tomlein@gmail.com>
2022-04-05 12:00:57 +0200

    Update configuration for wdio

--- trackers/javascript-tracker/test/wdio.default.conf.ts
+++ trackers/javascript-tracker/test/wdio.default.conf.ts
@@ -5,7 +5,10 @@ export const config = {
   waitforTimeout: 30000,
   connectionRetryTimeout: 120000,
   connectionRetryCount: 3,
-  specFileRetries: 1,
+  specFileRetries: 2,
+  specFileRetriesDelay: 60, // Delay in seconds between the spec file retry attempts
+  specFileRetriesDeferred: false, // Defer retries to the end of the queue
+  maxInstances: 5, // Maximum number of total parallel running workers
   framework: 'jasmine',
   reporters: ['spec'],
   jasmineOpts: {
