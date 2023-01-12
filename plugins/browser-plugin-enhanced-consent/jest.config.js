94934de9297cfbbc280c89c962292ee293f42de6
Peter Perlepes <p.perlepes@gmail.com>
2023-01-12 15:51:10 +0200

    Upgrade UUID to 8.3.2
    (close #1138)

--- plugins/browser-plugin-enhanced-consent/jest.config.js
+++ plugins/browser-plugin-enhanced-consent/jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
   reporters: ['jest-standard-reporter'],
+  setupFilesAfterEnv: ['../../setupTestGlobals.ts'],
   testEnvironment: 'jest-environment-jsdom-global',
 };
