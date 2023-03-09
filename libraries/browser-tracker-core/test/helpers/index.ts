51dc1128d21d056353bd347fa12038beb723a0c8
Peter Perlepes <p.perlepes@gmail.com>
2023-03-09 10:01:04 +0200

    Change default tracker initialization parameters for JS/Browser v4
    #1144

--- libraries/browser-tracker-core/test/helpers/index.ts
+++ libraries/browser-tracker-core/test/helpers/index.ts
@@ -60,7 +60,7 @@ export function createTestIdCookie(params: Partial<CreateTestIdCookie>) {
   const { domainHash } = cookieParams;
   // @ts-expect-error
   delete cookieParams.domainHash;
-  return `_sp_id.${domainHash}=${Object.values(cookieParams).join('.')}; Expires=; Path=/; SameSite=None; Secure;`;
+  return `_sp_id.${domainHash}=${Object.values(cookieParams).join('.')}; Expires=; Path=/; SameSite=Lax; Secure;`;
 }
 
 interface CreateTestSessionIdCookie {
@@ -72,7 +72,7 @@ interface CreateTestSessionIdCookie {
  */
 export function createTestSessionIdCookie(params?: CreateTestSessionIdCookie) {
   const domainHash = DEFAULT_DOMAIN_HASH || params?.domainHash;
-  return `_sp_ses.${domainHash}=*; Expires=; Path=/; SameSite=None; Secure;`;
+  return `_sp_ses.${domainHash}=*; Expires=; Path=/; SameSite=Lax; Secure;`;
 }
 
 export function createTracker(configuration?: TrackerConfiguration) {
