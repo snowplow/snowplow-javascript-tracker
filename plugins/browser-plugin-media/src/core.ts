e3c3c312e6f87b4953701ec89ba1d835b7568d3e
Matúš Tomlein <matus.tomlein@gmail.com>
2023-05-02 15:38:14 +0200

    Simplify constructing event and entity bodies

--- plugins/browser-plugin-media/src/core.ts
+++ plugins/browser-plugin-media/src/core.ts
@@ -18,28 +18,28 @@ export function buildMediaPlayerEvent(event: MediaEvent): SelfDescribingJson {
 export function buildMediaPlayerEntity(mediaPlayer: MediaPlayer): SelfDescribingJson {
   return {
     schema: MEDIA_PLAYER_SCHEMA,
-    data: removeEmptyProperties({ ...mediaPlayer }),
+    data: removeEmptyProperties(mediaPlayer),
   };
 }
 
 export function buildMediaPlayerSessionEntity(session: MediaSession): SelfDescribingJson {
   return {
     schema: MEDIA_PLAYER_SESSION_SCHEMA,
-    data: removeEmptyProperties({ ...session }),
+    data: removeEmptyProperties(session),
   };
 }
 
 export function buildMediaPlayerAdEntity(ad: MediaAd): SelfDescribingJson {
   return {
     schema: MEDIA_PLAYER_AD_SCHEMA,
-    data: removeEmptyProperties({ ...ad }),
+    data: removeEmptyProperties(ad),
   };
 }
 
 export function buildMediaPlayerAdBreakEntity(adBreak: MediaAdBreak): SelfDescribingJson {
   return {
     schema: MEDIA_PLAYER_AD_BREAK_SCHEMA,
-    data: removeEmptyProperties({ ...adBreak }),
+    data: removeEmptyProperties(adBreak),
   };
 }
 
@@ -47,13 +47,12 @@ export function buildMediaPlayerAdBreakEntity(adBreak: MediaAdBreak): SelfDescri
  * Returns a copy of a JSON with undefined and null properties removed
  *
  * @param event - Object to clean
- * @param exemptFields - Set of fields which should not be removed even if empty
  * @returns A cleaned copy of eventJson
  */
 function removeEmptyProperties(event: Record<string, unknown>): Record<string, unknown> {
   const ret: Record<string, unknown> = {};
   for (const k in event) {
-    if (event[k] !== null && typeof event[k] !== 'undefined') {
+    if (event[k] != null) {
       ret[k] = event[k];
     }
   }
