03b21c688efc387026395dd8b574d3ed6821a90c
Matúš Tomlein <matus.tomlein@gmail.com>
2023-08-07 16:27:38 +0200

    Update version of the player schema used in the media plugin to 2-0-0 (close #1222)

--- plugins/browser-plugin-media/src/schemata.ts
+++ plugins/browser-plugin-media/src/schemata.ts
@@ -2,7 +2,7 @@ import { MediaEventType } from './types';
 
 const MEDIA_SCHEMA_PREFIX = 'iglu:com.snowplowanalytics.snowplow.media/';
 const MEDIA_SCHEMA_SUFFIX = '/jsonschema/1-0-0';
-export const MEDIA_PLAYER_SCHEMA = MEDIA_SCHEMA_PREFIX + 'player' + MEDIA_SCHEMA_SUFFIX;
+export const MEDIA_PLAYER_SCHEMA = 'iglu:com.snowplowanalytics.snowplow/media_player/jsonschema/2-0-0';
 export const MEDIA_SESSION_SCHEMA = MEDIA_SCHEMA_PREFIX + 'session' + MEDIA_SCHEMA_SUFFIX;
 export const MEDIA_AD_SCHEMA = MEDIA_SCHEMA_PREFIX + 'ad' + MEDIA_SCHEMA_SUFFIX;
 export const MEDIA_AD_BREAK_SCHEMA = MEDIA_SCHEMA_PREFIX + 'ad_break' + MEDIA_SCHEMA_SUFFIX;
