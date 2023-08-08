ec14caf604e267de710d1bc853f6e64a3b82b43d
Matúš Tomlein <matus.tomlein@gmail.com>
2023-08-08 10:07:48 +0200

    Add a note about the player schema in a comment

--- plugins/browser-plugin-media/src/schemata.ts
+++ plugins/browser-plugin-media/src/schemata.ts
@@ -2,6 +2,7 @@ import { MediaEventType } from './types';
 
 const MEDIA_SCHEMA_PREFIX = 'iglu:com.snowplowanalytics.snowplow.media/';
 const MEDIA_SCHEMA_SUFFIX = '/jsonschema/1-0-0';
+// NOTE: The player schema has a different vendor than the other media schemas because it builds on an older version of the same schema. Versions 3.12 to 3.13.1 of the media plugin used a conflicting schema URI which has since been removed from Iglu Central.
 export const MEDIA_PLAYER_SCHEMA = 'iglu:com.snowplowanalytics.snowplow/media_player/jsonschema/2-0-0';
 export const MEDIA_SESSION_SCHEMA = MEDIA_SCHEMA_PREFIX + 'session' + MEDIA_SCHEMA_SUFFIX;
 export const MEDIA_AD_SCHEMA = MEDIA_SCHEMA_PREFIX + 'ad' + MEDIA_SCHEMA_SUFFIX;
