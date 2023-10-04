ee07554f61e85e758cdf67e3150278c6f2bd4a81
Greg Leonard <45019882+greg-el@users.noreply.github.com>
2023-10-04 19:16:57 +0100

    Add support for callbacks on failed/successful event sending (close #1262)

--- libraries/browser-tracker-core/test/out_queue.test.ts
+++ libraries/browser-tracker-core/test/out_queue.test.ts
@@ -30,12 +30,22 @@
 
 import { OutQueueManager, OutQueue } from '../src/tracker/out_queue';
 import { SharedState } from '../src/state';
+import { EventBatch, RequestFailure } from '../src/tracker/types';
 
 const readPostQueue = () => {
   return JSON.parse(
     window.localStorage.getItem('snowplowOutQueue_sp_post2') ?? fail('Unable to find local storage queue')
   );
 };
+
+const readGetQueue = () =>
+  JSON.parse(window.localStorage.getItem('snowplowOutQueue_sp_get') ?? fail('Unable to find local storage queue'));
+
+const getQuerystring = (p: object) =>
+  '?' +
+  Object.entries(p)
+    .map(([k, v]) => k + '=' + encodeURIComponent(v))
+    .join('&');
 
 describe('OutQueueManager', () => {
   const maxQueueSize = 2;
@@ -57,9 +67,10 @@ describe('OutQueueManager', () => {
     jest.spyOn(window, 'XMLHttpRequest').mockImplementation(() => xhrMock as XMLHttpRequest);
   });
 
-  const respondMockRequest = (status: number) => {
+  const respondMockRequest = (status: number, statusText: string = '') => {
     (xhrMock as any).status = status;
     (xhrMock as any).response = '';
+    (xhrMock as any).statusText = statusText;
     (xhrMock as any).readyState = 4;
     (xhrMock as any).onreadystatechange();
   };
@@ -169,11 +180,6 @@ describe('OutQueueManager', () => {
 
   describe('GET requests', () => {
     var getOutQueue: (maxGetBytes: number) => OutQueue;
-    const getQuerystring = (p: object) =>
-      '?' +
-      Object.entries(p)
-        .map(([k, v]) => k + '=' + encodeURIComponent(v))
-        .join('&');
 
     beforeEach(() => {
       getOutQueue = (maxGetBytes) =>
@@ -419,6 +425,269 @@ describe('OutQueueManager', () => {
         expect(retrievedQueue).toHaveLength(0);
         done();
       }, 20);
+    });
+  });
+
+  type createQueueArgs = {
+    method: string;
+    onSuccess?: (data: EventBatch) => void;
+    onFailure?: (data: RequestFailure) => void;
+    maxPostBytes?: number;
+    maxGetBytes?: number;
+  };
+
+  const createQueue = (args: createQueueArgs) =>
+    OutQueueManager(
+      'sp',
+      new SharedState(),
+      true,
+      args.method,
+      '/com.snowplowanalytics.snowplow/tp2',
+      1,
+      args.maxPostBytes ?? 40000,
+      args.maxGetBytes ?? 0,
+      true,
+      maxQueueSize,
+      5000,
+      false,
+      {},
+      true,
+      [],
+      [],
+      '',
+      false,
+      args.onSuccess,
+      args.onFailure
+    );
+
+  describe('onRequestSuccess', () => {
+    const request = { e: 'pv', eid: '65cb78de-470c-4764-8c10-02bd79477a3a' };
+
+    describe('POST requests', () => {
+      const method = 'POST';
+
+      it('should fire on a successful request', () => {
+        const callbackStorage: EventBatch[] = [];
+        const onSuccess = (e: EventBatch) => {
+          callbackStorage.push(e);
+        };
+
+        const postQueue = createQueue({ method, onSuccess });
+        postQueue.enqueueRequest(request, 'http://example.com');
+
+        expect(readPostQueue()).toHaveLength(1);
+
+        respondMockRequest(200);
+
+        expect(readPostQueue()).toHaveLength(0);
+        expect(callbackStorage).toHaveLength(1);
+
+        let dataFromCallback = callbackStorage[0][0] as Record<string, unknown>;
+        expect(dataFromCallback.e).toEqual(request.e);
+        expect(dataFromCallback.eid).toEqual(request.eid);
+        expect(dataFromCallback.stm).toMatch(/\d{13}/);
+      });
+
+      // Oversized events don't get placed in the queue, but the callback should still fire
+      it('should fire on a successful oversized request', () => {
+        const callbackStorage: EventBatch[] = [];
+        const onSuccess = (e: EventBatch) => {
+          callbackStorage.push(e);
+        };
+
+        const postQueue = createQueue({ method, onSuccess, maxPostBytes: 1 });
+        postQueue.enqueueRequest(request, 'http://example.com');
+
+        respondMockRequest(200);
+
+        expect(callbackStorage).toHaveLength(1);
+
+        let dataFromCallback = callbackStorage[0][0] as Record<string, unknown>;
+        expect(dataFromCallback.e).toEqual(request.e);
+        expect(dataFromCallback.eid).toEqual(request.eid);
+        expect(dataFromCallback.stm).toMatch(/\d{13}/);
+      });
+    });
+
+    describe('GET requests', () => {
+      const method = 'GET';
+
+      it('should fire on a successful request', () => {
+        let callbackStorage: EventBatch[] = [];
+        const onSuccess = (e: EventBatch) => {
+          callbackStorage.push(e);
+        };
+
+        const getQueue = createQueue({ method, onSuccess });
+        getQueue.enqueueRequest(request, 'http://example.com');
+
+        expect(readGetQueue()).toHaveLength(1);
+
+        respondMockRequest(200);
+
+        expect(readGetQueue()).toHaveLength(0);
+        expect(callbackStorage).toHaveLength(1);
+
+        let dataFromCallback = callbackStorage[0][0] as string;
+        expect(dataFromCallback).toMatch(/\?stm=\d{13}&e=pv&eid=65cb78de-470c-4764-8c10-02bd79477a3a/);
+      });
+
+      // A single oversized events means no queue, but the callback should still fire
+      it('should fire the onRequestSuccess on a successful oversized request', () => {
+        let callbackStorage: EventBatch[] = [];
+        const onSuccess = (e: EventBatch) => {
+          callbackStorage.push(e);
+        };
+
+        const getQueue = createQueue({ method, onSuccess });
+        getQueue.enqueueRequest(request, 'http://example.com');
+
+        respondMockRequest(200);
+
+        expect(callbackStorage).toHaveLength(1);
+
+        let dataFromCallback = callbackStorage[0][0] as string;
+        expect(dataFromCallback).toMatch(/\?stm=\d{13}&e=pv&eid=65cb78de-470c-4764-8c10-02bd79477a3a/);
+      });
+    });
+  });
+
+  describe('onRequestFailure', () => {
+    const request = { e: 'pv', eid: '65cb78de-470c-4764-8c10-02bd79477a3a' };
+
+    const createQueue = (args: createQueueArgs) =>
+      OutQueueManager(
+        'sp',
+        new SharedState(),
+        true,
+        args.method,
+        '/com.snowplowanalytics.snowplow/tp2',
+        1,
+        40000,
+        0,
+        true,
+        maxQueueSize,
+        5000,
+        false,
+        {},
+        true,
+        [],
+        [500],
+        '',
+        false,
+        args.onSuccess,
+        args.onFailure
+      );
+
+    describe('POST requests', () => {
+      const method = 'POST';
+
+      it('should fire on a failed request', () => {
+        const callbackStorage: RequestFailure[] = [];
+        const onFailure = (e: RequestFailure) => {
+          callbackStorage.push(e);
+        };
+
+        const postQueue = createQueue({ method, onFailure });
+        postQueue.enqueueRequest(request, 'http://example.com');
+
+        expect(readPostQueue()).toHaveLength(1);
+
+        respondMockRequest(500, 'Internal Server Error');
+
+        expect(readPostQueue()).toHaveLength(0);
+        expect(callbackStorage).toHaveLength(1);
+
+        let dataFromCallback = callbackStorage[0] as RequestFailure;
+
+        const event = dataFromCallback.events[0] as Record<string, unknown>;
+        expect(event.e).toEqual(request.e);
+        expect(event.eid).toEqual(request.eid);
+        expect(event.stm).toMatch(/\d{13}/);
+
+        expect(dataFromCallback.status).toEqual(500);
+        expect(dataFromCallback.message).toEqual('Internal Server Error');
+        expect(dataFromCallback.willRetry).toEqual(false);
+      });
+
+      // A single oversized events means no queue, but the callback should still fire
+      it('should fire on a failed oversized request', () => {
+        const callbackStorage: RequestFailure[] = [];
+        const onFailure = (e: RequestFailure) => {
+          callbackStorage.push(e);
+        };
+
+        const postQueue = createQueue({ method, onFailure, maxPostBytes: 1 });
+        postQueue.enqueueRequest(request, 'http://example.com');
+
+        respondMockRequest(0, 'Request failed');
+
+        expect(callbackStorage).toHaveLength(1);
+
+        let dataFromCallback = callbackStorage[0].events[0] as Record<string, unknown>;
+        expect(dataFromCallback.e).toEqual(request.e);
+        expect(dataFromCallback.eid).toEqual(request.eid);
+
+        // The payload will have had `stm` added to it
+        expect(dataFromCallback.stm).toMatch(/\d{13}/);
+        expect(callbackStorage[0].status).toEqual(0);
+        expect(callbackStorage[0].message).toEqual('Request failed');
+      });
+    });
+
+    describe('GET requests', () => {
+      const method = 'GET';
+
+      it('should fire on a failed request', () => {
+        let callbackStorage: RequestFailure[] = [];
+        const onFailure = (e: RequestFailure) => {
+          callbackStorage.push(e);
+        };
+
+        const getQueue = createQueue({ method, onFailure });
+        getQueue.enqueueRequest(request, 'http://example.com');
+
+        expect(readGetQueue()).toHaveLength(1);
+
+        respondMockRequest(500, 'Internal Server Error');
+
+        expect(readGetQueue()).toHaveLength(0);
+        expect(callbackStorage).toHaveLength(1);
+
+        let dataFromCallback = callbackStorage[0] as RequestFailure;
+
+        expect(dataFromCallback.events[0]).toMatch(/\?stm=\d{13}&e=pv&eid=65cb78de-470c-4764-8c10-02bd79477a3a/);
+
+        expect(dataFromCallback.status).toEqual(500);
+        expect(dataFromCallback.message).toEqual('Internal Server Error');
+        expect(dataFromCallback.willRetry).toEqual(false);
+      });
+
+      // A single oversized events means no queue, but the callback should still fire
+      it('should fire on a failed oversized request', () => {
+        let callbackStorage: RequestFailure[] = [];
+        const onFailure = (e: RequestFailure) => {
+          callbackStorage.push(e);
+        };
+
+        const getQueue = createQueue({ method, onFailure, maxPostBytes: 1 });
+        getQueue.enqueueRequest(request, 'http://example.com');
+
+        expect(readGetQueue()).toHaveLength(1);
+
+        respondMockRequest(500, 'Internal Server Error');
+
+        expect(readGetQueue()).toHaveLength(0);
+        expect(callbackStorage).toHaveLength(1);
+
+        let dataFromCallback = callbackStorage[0] as RequestFailure;
+
+        expect(dataFromCallback.events[0]).toMatch(/\?stm=\d{13}&e=pv&eid=65cb78de-470c-4764-8c10-02bd79477a3a/);
+
+        expect(dataFromCallback.status).toEqual(500);
+        expect(dataFromCallback.message).toEqual('Internal Server Error');
+        expect(dataFromCallback.willRetry).toEqual(false);
+      });
     });
   });
 });
