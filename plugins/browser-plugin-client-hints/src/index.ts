67a22e412abe1819b07a28002c40117a11576c7a
adatzer <ada@snowplowanalytics.com>
2023-11-11 13:09:11 +0200

    Drop invalid client hints (close #1264)

--- plugins/browser-plugin-client-hints/src/index.ts
+++ plugins/browser-plugin-client-hints/src/index.ts
@@ -1,33 +1,3 @@
-/*
- * Copyright (c) 2022 Snowplow Analytics Ltd, 2010 Anthon Pang
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- * 3. Neither the name of the copyright holder nor the names of its
- *    contributors may be used to endorse or promote products derived from
- *    this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 import { BrowserPlugin } from '@snowplow/browser-tracker-core';
 import { HttpClientHints } from './contexts';
 
@@ -63,9 +33,42 @@ let uaClientHints: HttpClientHints;
 function forceArray<T>(array: T[] | Record<string, T>): T[] {
   if (Array.isArray(array)) return array;
 
-  return Object.keys(array).map((e) => {
-    return array[e];
+  if (Object.prototype.toString.call(array) === '[object Object]') {
+    return Object.keys(array).map((e) => {
+      return array[e];
+    });
+  }
+
+  return [];
+}
+
+/**
+ * Returns the client-hints brands, ensuring no additional properties.
+ */
+function getBrands(brands: Array<NavigatorUABrandVersion>): Array<NavigatorUABrandVersion> {
+  return brands.map((b) => {
+    const { brand, version } = b;
+    return { brand, version };
   });
+}
+
+/**
+ * Validates whether userAgentData is compliant to the client-hints interface.
+ * https://wicg.github.io/ua-client-hints/#interface
+ */
+function validClientHints(hints: HttpClientHints): boolean {
+  if (!hints || typeof hints.isMobile !== 'boolean' || !Array.isArray(hints.brands)) {
+    return false;
+  }
+
+  if (
+    hints.brands.length === 0 ||
+    hints.brands.some((brand) => typeof brand.brand !== 'string' || typeof brand.version !== 'string')
+  ) {
+    return false;
+  }
+
+  return true;
 }
 
 /**
@@ -75,22 +78,28 @@ function forceArray<T>(array: T[] | Record<string, T>): T[] {
 export function ClientHintsPlugin(includeHighEntropy?: boolean): BrowserPlugin {
   const populateClientHints = () => {
     const navigatorAlias = navigator;
+    const uaData = navigatorAlias.userAgentData;
 
-    if (navigatorAlias.userAgentData) {
-      uaClientHints = {
-        isMobile: navigatorAlias.userAgentData.mobile,
-        brands: forceArray(navigatorAlias.userAgentData.brands),
+    if (uaData) {
+      let candidateHints: HttpClientHints;
+      candidateHints = {
+        isMobile: uaData.mobile,
+        brands: getBrands(forceArray(uaData.brands)),
       };
-      if (includeHighEntropy && navigatorAlias.userAgentData.getHighEntropyValues) {
-        navigatorAlias.userAgentData
+      if (includeHighEntropy && uaData.getHighEntropyValues) {
+        uaData
           .getHighEntropyValues(['platform', 'platformVersion', 'architecture', 'model', 'uaFullVersion'])
           .then((res) => {
-            uaClientHints.architecture = res.architecture;
-            uaClientHints.model = res.model;
-            uaClientHints.platform = res.platform;
-            uaClientHints.uaFullVersion = res.uaFullVersion;
-            uaClientHints.platformVersion = res.platformVersion;
+            candidateHints.architecture = res.architecture;
+            candidateHints.model = res.model;
+            candidateHints.platform = res.platform;
+            candidateHints.uaFullVersion = res.uaFullVersion;
+            candidateHints.platformVersion = res.platformVersion;
           });
+      }
+
+      if (validClientHints(candidateHints)) {
+        uaClientHints = candidateHints;
       }
     }
   };
