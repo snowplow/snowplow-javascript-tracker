c5cf56d149553ae2f915e43d987f7443d1c5300b
Jethro Nederhof <jethro.nederhof@snowplowanalytics.com>
2024-09-11 11:50:16 +1000

    Migrate to v2 schemas for youtube-tracking plugin
    See PE-5753.

--- plugins/browser-plugin-youtube-tracking/src/helperFunctions.ts
+++ plugins/browser-plugin-youtube-tracking/src/helperFunctions.ts
@@ -11,25 +11,25 @@ export function isElementFullScreen(mediaId: string): boolean {
 export function addUrlParam(url: string, key: string, value: string) {
   const urlParams = parseUrlParams(url);
   urlParams[key] = value;
-  return `${url}?${urlParamsToString(urlParams)}`;
+
+  return `${url.split('?').shift()!}?${urlParamsToString(urlParams)}`;
 }
 
 export function parseUrlParams(url: string): Record<string, string> {
   const params: Record<string, string> = {};
-  const urlParams = url.split('?')[1];
+  const urlParams = url.split('?').slice(1).join('?');
   if (!urlParams) return params;
   urlParams.split('&').forEach((p) => {
     const param = p.split('=');
-    params[param[0]] = param[1];
+    const key = decodeURIComponent(param.shift()!);
+    params[key] = decodeURIComponent(param.join('='));
   });
   return params;
 }
 
 function urlParamsToString(urlParams: Record<string, string>) {
   // convert an object of url parameters to a string
-  let params = '';
-  Object.keys(urlParams).forEach((p) => {
-    params += `${p}=${urlParams[p]}&`;
-  });
-  return params.slice(0, -1);
+  return Object.keys(urlParams)
+    .map((p) => `${encodeURIComponent(p)}=${encodeURIComponent(urlParams[p])}`)
+    .join('&');
 }
