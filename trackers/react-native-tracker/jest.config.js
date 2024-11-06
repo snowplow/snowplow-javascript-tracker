853d39649a86adb145e84f6f26750ad7c8d68bc1
Matúš Tomlein <matus.tomlein@gmail.com>
2024-11-06 10:20:55 +0100

    Downgrade jest version to 28 to fix tests

--- trackers/react-native-tracker/jest.config.js
+++ trackers/react-native-tracker/jest.config.js
@@ -1,7 +1,5 @@
 module.exports = {
   preset: 'react-native',
   transformIgnorePatterns: [],
-  globals: {
-    crypto: require('crypto'),
-  },
+  setupFilesAfterEnv: ['../../setupTestGlobals.ts'],
 };
