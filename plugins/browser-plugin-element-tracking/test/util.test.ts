38b0e41768cec69677323c08b7ea231be4978ae3
Jethro Nederhof <jethro.nederhof@snowplowanalytics.com>
2024-11-27 18:55:43 +1100

    More tests

--- plugins/browser-plugin-element-tracking/test/util.test.ts
+++ plugins/browser-plugin-element-tracking/test/util.test.ts
@@ -17,6 +17,28 @@ const TEMPLATE_CONFIG: Configuration = {
   contents: [],
 };
 
+const TEMPLATE_DOMRECT: DOMRectReadOnly = {
+  bottom: 0,
+  top: 0,
+  left: 0,
+  right: 0,
+  height: 0,
+  width: 0,
+  x: 0,
+  y: 0,
+  toJSON() {},
+};
+
+const TEMPLATE_INTERSECTION: IntersectionObserverEntry = {
+  time: 0,
+  isIntersecting: true,
+  target: document.body,
+  rootBounds: null,
+  boundingClientRect: TEMPLATE_DOMRECT,
+  intersectionRatio: 0,
+  intersectionRect: TEMPLATE_DOMRECT,
+};
+
 describe('utils', () => {
   describe('defineBoundaries', () => {
     it.each([
@@ -129,5 +151,95 @@ describe('utils', () => {
         expect(results).toEqual(elements.concat([addition]));
       });
     });
+  });
+
+  describe('shouldTrackExpose', () => {
+    it.each([
+      ['frequency: never', { when: 'never' as const }, {}, false],
+      ['not intersecting', { when: 'always' as const }, { isIntersecting: false }, false],
+      ['intersecting', { when: 'always' as const }, { isIntersecting: true }, true],
+      ['zero-size', { when: 'always' as const, minSize: 1 }, { isIntersecting: true }, false],
+      [
+        'partial view',
+        { when: 'always' as const, minPercentage: 1 },
+        {
+          intersectionRatio: (100 * 100) / (200 * 200),
+          intersectionRect: { ...TEMPLATE_DOMRECT, width: 100, height: 100 },
+          boundingClientRect: { ...TEMPLATE_DOMRECT, width: 200, height: 200 },
+        },
+        false,
+      ],
+      [
+        'sufficent view',
+        { when: 'always' as const, minPercentage: (200 * 160) / (200 * 200) },
+        {
+          intersectionRatio: (200 * 160) / (200 * 200),
+          intersectionRect: { ...TEMPLATE_DOMRECT, width: 200, height: 160 },
+          boundingClientRect: { ...TEMPLATE_DOMRECT, width: 200, height: 200 },
+        },
+        true,
+      ],
+      [
+        'insufficent view',
+        { when: 'always' as const, minPercentage: (200 * 160) / (200 * 200) },
+        {
+          intersectionRatio: (200 * 159) / (200 * 200),
+          intersectionRect: { ...TEMPLATE_DOMRECT, width: 200, height: 159 },
+          boundingClientRect: { ...TEMPLATE_DOMRECT, width: 200, height: 200 },
+        },
+        false,
+      ],
+      [
+        'shrunken element',
+        { when: 'always' as const, minPercentage: (200 * 160) / (200 * 200), boundaryPixels: -100 },
+        {
+          intersectionRatio: (200 * 159) / (200 * 200),
+          intersectionRect: { ...TEMPLATE_DOMRECT, width: 200, height: 159 },
+          boundingClientRect: { ...TEMPLATE_DOMRECT, width: 200, height: 200 },
+        },
+        true,
+      ],
+      [
+        'enlarged element - possible',
+        { when: 'always' as const, minPercentage: 0.1, boundaryPixels: 100 },
+        {
+          intersectionRatio: (200 * 200) / (200 * 200),
+          intersectionRect: { ...TEMPLATE_DOMRECT, width: 200, height: 200 },
+          boundingClientRect: { ...TEMPLATE_DOMRECT, width: 200, height: 200 },
+        },
+        true,
+      ],
+      [
+        'enlarged element - impossible',
+        { when: 'always' as const, minPercentage: 1, boundaryPixels: 100 },
+        {
+          intersectionRatio: (200 * 200) / (200 * 200),
+          intersectionRect: { ...TEMPLATE_DOMRECT, width: 200, height: 200 },
+          boundingClientRect: { ...TEMPLATE_DOMRECT, width: 200, height: 200 },
+        },
+        false,
+      ],
+    ])(
+      '%s',
+      (
+        _: string,
+        expose: Partial<Configuration['expose']>,
+        entry: Partial<IntersectionObserverEntry>,
+        shouldPass: boolean
+      ) => {
+        expect(
+          shouldTrackExpose(
+            {
+              ...TEMPLATE_CONFIG,
+              expose: { ...TEMPLATE_CONFIG.expose, ...expose },
+            },
+            {
+              ...TEMPLATE_INTERSECTION,
+              ...entry,
+            }
+          )
+        ).toBe(shouldPass);
+      }
+    );
   });
 });
