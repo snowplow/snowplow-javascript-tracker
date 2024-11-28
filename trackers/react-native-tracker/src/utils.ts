d0c64de6312a3d2419aae91c5c1041a3ce7f7d90
Matus Tomlein <matus.tomlein@gmail.com>
2024-11-28 11:50:25 +0100

    Update trackers/react-native-tracker/src/utils.ts
    Co-authored-by: Miranda Wilson <miranda@snowplow.io>

--- trackers/react-native-tracker/src/utils.ts
+++ trackers/react-native-tracker/src/utils.ts
@@ -1,7 +1,7 @@
 import { PayloadBuilder } from '@snowplow/tracker-core';
 
 // Returns the "useful" schema, i.e. what would someone want to use to identify events.
-// For some events this is the 'e' property but for unstructured events, this is the
+// For some events this is the 'e' property but for self-describing events, this is the
 // 'schema' from the 'ue_px' field.
 export function getUsefulSchema(sb: PayloadBuilder): string {
   let eventJson = sb.getJson();
