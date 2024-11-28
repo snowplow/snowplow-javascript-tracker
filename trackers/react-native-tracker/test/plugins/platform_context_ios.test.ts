9d38ab8649acb92f0c342191b27e48364bb9aeaa
Matúš Tomlein <matus.tomlein@gmail.com>
2024-11-28 10:45:12 +0100

    Add a platform context plugin to the React Native tracker to track device information
    # Conflicts:
    #	api-docs/docs/react-native-tracker/markdown/react-native-tracker.reactnativetracker.md
    #	api-docs/docs/react-native-tracker/react-native-tracker.api.md
    #	trackers/react-native-tracker/src/constants.ts
    #	trackers/react-native-tracker/src/tracker.ts
    #	trackers/react-native-tracker/test/tracker.test.ts

---
+++ trackers/react-native-tracker/test/plugins/platform_context_ios.test.ts
@@ -0,0 +1,162 @@
+import { buildPageView, Payload, trackerCore } from '@snowplow/tracker-core';
+import { newPlatformContextPlugin } from '../../src/plugins/platform_context';
+import { NativeModules } from 'react-native';
+import { MOBILE_CONTEXT_SCHEMA } from '../../src/constants';
+
+describe('PlatformContextPlugin on iOS', () => {
+  beforeAll(() => {
+    jest.mock('react-native/Libraries/Utilities/Platform', () => ({
+      OS: 'ios',
+      Version: '18.0',
+      constants: {
+        forceTouchAvailable: false,
+        interfaceIdiom: 'phone',
+        isMacCatalyst: false,
+        isTesting: false,
+        osVersion: '18.0',
+        systemName: 'iOS',
+      },
+      isMacCatalyst: false,
+      isPad: false,
+      isTV: false,
+      isVision: false,
+      select: () => null,
+    }));
+    NativeModules.SettingsManager = {
+      settings: {
+        AppleLanguages: ['en-GB'],
+      },
+    };
+  });
+
+  afterAll(() => {
+    jest.clearAllMocks();
+  });
+
+  it('adds platform context to events', async () => {
+    const sessionPlugin = await newPlatformContextPlugin();
+
+    const payloads: Payload[] = [];
+    const tracker = trackerCore({
+      corePlugins: [sessionPlugin.plugin],
+      callback: (pb) => payloads.push(pb.build()),
+      base64: false,
+    });
+    tracker.track(buildPageView({ pageUrl: 'http://localhost' }));
+
+    expect(payloads.length).toBe(1);
+    const [payload] = payloads;
+    expect(payload?.co).toBeDefined();
+    expect(payload?.co).toContain(MOBILE_CONTEXT_SCHEMA);
+    expect(payload?.co).toContain('"iOS"');
+    expect(payload?.co).toContain('"18.0"');
+    expect(payload?.co).toContain('"en-GB"');
+  });
+
+  it('does not add platform context to events if disabled', async () => {
+    const sessionPlugin = await newPlatformContextPlugin({ platformContext: false });
+
+    const payloads: Payload[] = [];
+    const tracker = trackerCore({
+      corePlugins: [sessionPlugin.plugin],
+      callback: (pb) => payloads.push(pb.build()),
+      base64: false,
+    });
+    tracker.track(buildPageView({ pageUrl: 'http://localhost' }));
+
+    expect(payloads.length).toBe(1);
+    const [payload] = payloads;
+    expect(payload?.co ?? '').not.toContain(MOBILE_CONTEXT_SCHEMA);
+  });
+
+  it('skips properties not enabled', async () => {
+    const sessionPlugin = await newPlatformContextPlugin({ platformContextProperties: [] });
+
+    const payloads: Payload[] = [];
+    const tracker = trackerCore({
+      corePlugins: [sessionPlugin.plugin],
+      callback: (pb) => payloads.push(pb.build()),
+      base64: false,
+    });
+    tracker.track(buildPageView({ pageUrl: 'http://localhost' }));
+
+    expect(payloads.length).toBe(1);
+    const [payload] = payloads;
+    expect(payload?.co).toBeDefined();
+    expect(payload?.co).toContain(MOBILE_CONTEXT_SCHEMA);
+    expect(payload?.co).toContain('"iOS"');
+    expect(payload?.co).not.toContain('"en-GB"');
+  });
+
+  it('overrides properties using the retriever', async () => {
+    const sessionPlugin = await newPlatformContextPlugin({
+      platformContextRetriever: {
+        getOsType: () => Promise.resolve('Android'),
+        getOsVersion: () => Promise.resolve('11.0'),
+        getDeviceModel: () => Promise.resolve('Galaxy S21'),
+        getDeviceManufacturer: () => Promise.resolve('Samsung'),
+        getCarrier: () => Promise.resolve('Vodafone'),
+        getNetworkType: () => Promise.resolve('wifi'),
+        getNetworkTechnology: () => Promise.resolve('5G'),
+        getAppleIdfa: () => Promise.resolve('my-idfa'),
+        getAppleIdfv: () => Promise.resolve('my-idfv'),
+        getAvailableStorage: () => Promise.resolve(1000000000),
+        getTotalStorage: () => Promise.resolve(2000000000),
+        getPhysicalMemory: () => Promise.resolve(8000000000),
+        getAppAvailableMemory: () => Promise.resolve(2000000000),
+        getBatteryLevel: () => Promise.resolve(50),
+        getBatteryState: () => Promise.resolve('full'),
+        getLowPowerMode: () => Promise.resolve(false),
+        isPortrait: () => Promise.resolve(true),
+        getResolution: () => Promise.resolve('1920x1080'),
+        getScale: () => Promise.resolve(2),
+        getLanguage: () => Promise.resolve('en-US'),
+        getAndroidIdfa: () => Promise.resolve('my-android-idfa'),
+        getSystemAvailableMemory: () => Promise.resolve(2000000000),
+        getAppSetId: () => Promise.resolve('my-app-set'),
+        getAppSetIdScope: () => Promise.resolve('my-app-set-scope'),
+      },
+    });
+
+    const payloads: Payload[] = [];
+    const tracker = trackerCore({
+      corePlugins: [sessionPlugin.plugin],
+      callback: (pb) => payloads.push(pb.build()),
+      base64: false,
+    });
+    tracker.track(buildPageView({ pageUrl: 'http://localhost' }));
+
+    expect(payloads.length).toBe(1);
+    const [payload] = payloads;
+    expect(payload?.co).toBeDefined();
+    const entities = JSON.parse(payload?.co as string).data;
+    const mobileContext = entities.find((entity: any) => entity.schema === MOBILE_CONTEXT_SCHEMA);
+    expect(mobileContext).toBeDefined();
+    expect(mobileContext.data).toMatchObject({
+      osType: 'Android',
+      osVersion: '11.0',
+      deviceModel: 'Galaxy S21',
+      deviceManufacturer: 'Samsung',
+      carrier: 'Vodafone',
+      networkType: 'wifi',
+      networkTechnology: '5G',
+      appleIdfa: 'my-idfa',
+      appleIdfv: 'my-idfv',
+      availableStorage: 1000000000,
+      totalStorage: 2000000000,
+      physicalMemory: 8000000000,
+      appAvailableMemory: 2000000000,
+      batteryLevel: 50,
+      batteryState: 'full',
+      lowPowerMode: false,
+      isPortrait: true,
+      resolution: '1920x1080',
+      scale: 2,
+      language: 'en-US',
+      androidIdfa: 'my-android-idfa',
+      systemAvailableMemory: 2000000000,
+      appSetId: 'my-app-set',
+      appSetIdScope: 'my-app-set-scope',
+    });
+  });
+});
