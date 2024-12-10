14b888067a9614bfa520a01d998ae1fe3740dc07
Jethro Nederhof <jethro.nederhof@snowplowanalytics.com>
2024-12-10 16:47:23 +1100

    Add stats tracking

--- plugins/browser-plugin-element-tracking/src/elementsState.ts
+++ plugins/browser-plugin-element-tracking/src/elementsState.ts
@@ -1,4 +1,5 @@
 import type { Configuration } from './configuration';
+import { Entities, type ElementStatisticsEntity } from './schemata';
 
 export enum ElementStatus {
   INITIAL,
@@ -37,6 +38,22 @@ type ElementState = {
    * The other matches for this element's selector we last saw, of which the element is/was at `lastPosition`-1 position.
    */
   matches: Set<Configuration>;
+  /**
+   * Smallest size dimensions seen so far for this element.
+   */
+  minSize?: [number, number];
+  /**
+   * Largest size dimensions seen so far for this element.
+   */
+  maxSize?: [number, number];
+  /**
+   * Largest vertical depth seen so far for this element, and the height the element was at that time.
+   */
+  maxDepth?: [number, number];
+  /**
+   * Number of times this element has entered viewport so far.
+   */
+  views: number;
 };
 
 /**
@@ -71,3 +88,67 @@ export function getState(target: Element, initial: Partial<ElementState> = {}): 
   }
 }
 
+const stateLabels: Record<ElementStatus, string> = {
+  [ElementStatus.CREATED]: 'exists',
+  [ElementStatus.DESTROYED]: 'destroyed',
+  [ElementStatus.EXPOSED]: 'on screen',
+  [ElementStatus.INITIAL]: 'unknown',
+  [ElementStatus.OBSCURED]: 'off screen',
+  [ElementStatus.PENDING]: 'awaiting time criteria',
+};
+
+const compareSize = (a: [number, number], b?: [number, number]) => {
+  if (b === undefined) return 0;
+  const asize = a[0] * a[1];
+  const bsize = b[0] * b[1];
+  return asize - bsize;
+};
+
+export function aggregateStats(
+  element_name: string,
+  target: Element,
+  index: number,
+  matches: number
+): ElementStatisticsEntity {
+  const state = getState(target);
+  const stateLabel = stateLabels[state.state];
+
+  const rect = target.getBoundingClientRect();
+  const curSize: [number, number] = [rect.width, rect.height];
+  const minSize = compareSize(curSize, state.minSize) <= 0 ? curSize : state.minSize!;
+  const maxSize = compareSize(curSize, state.maxSize) < 0 ? state.maxSize! : curSize;
+
+  const vpBottom = window.visualViewport
+    ? window.visualViewport.height + window.visualViewport.pageTop
+    : window.innerHeight + window.scrollY;
+  const curDepth: [number, number] = [Math.min(vpBottom, rect.top + rect.height) - rect.top, rect.height];
+  const maxDepth =
+    state.maxDepth === undefined || state.maxDepth[1] === 0
+      ? curDepth
+      : curDepth[1] === 0
+      ? state.maxDepth
+      : curDepth[0] / curDepth[1] > state.maxDepth[0] / state.maxDepth[1]
+      ? curDepth
+      : state.maxDepth;
+
+  Object.assign(state, { minSize, maxSize, maxDepth });
+
+  return {
+    schema: Entities.ELEMENT_STATISTICS,
+    data: {
+      element_name,
+      element_index: index,
+      element_matches: matches,
+      current_state: stateLabel,
+      min_size: minSize.join('x'),
+      current_size: curSize.join('x'),
+      max_size: maxSize.join('x'),
+      y_depth_percentage: curDepth[1] === 0 ? null : curDepth[0] / curDepth[1],
+      max_y_depth_percentage: maxDepth[1] === 0 ? null : maxDepth[0] / maxDepth[1],
+      max_y_depth: maxDepth.join('/'),
+      element_age_ms: performance.now() - (state.createdTs - performance.timeOrigin),
+      times_in_view: state.views,
+      total_time_visible_ms: state.elapsedVisibleMs,
+    },
+  };
+}
--- plugins/browser-plugin-element-tracking/test/components.test.ts
+++ plugins/browser-plugin-element-tracking/test/components.test.ts
@@ -18,6 +18,7 @@ describe('component detection', () => {
     state: 0,
     details: [],
     contents: [],
+    includeStats: [],
   };
 
   beforeAll(() => {
