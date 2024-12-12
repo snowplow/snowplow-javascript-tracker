ba623c38592b6ac9fabfc4d686ae9d2ea74fc13f
Jethro Nederhof <jethro.nederhof@snowplowanalytics.com>
2024-12-12 17:29:15 +1100

    Make tests v4 compatible

--- plugins/browser-plugin-element-tracking/jest.config.js
+++ plugins/browser-plugin-element-tracking/jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   preset: 'ts-jest',
   reporters: ['jest-standard-reporter'],
   testEnvironment: 'jest-environment-jsdom-global',
+  setupFilesAfterEnv: ['../../setupTestGlobals.ts'],
 };
