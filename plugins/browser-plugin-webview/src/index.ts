7750fef8985ab4a83d3694f9e890f251c7c35c44
Miranda Wilson <miranda@snowplow.io>
2025-01-16 16:20:51 +0000

    Add WebView plugin (#1402)
    * Add plugin based on my local webview tracker
    
    * Use webview tracker symlink
    
    * Correctly parse base64-encoded payloads
    
    * Parse string numbers
    
    * Add namespaces config option
    
    * Remove unused callback
    
    * Add docstring
    
    * Fix self-desc events
    
    * Use actual webview tracker
    
    * Run rush change

---
+++ plugins/browser-plugin-webview/src/index.ts
@@ -0,0 +1,79 @@
+import { BrowserPlugin, Payload } from '@snowplow/browser-tracker-core';
+import { hasMobileInterface, trackWebViewEvent } from '@snowplow/webview-tracker';
+import { Logger, SelfDescribingEvent, SelfDescribingJson } from '@snowplow/tracker-core';
+import { base64urldecode } from './utils';
+
+type WebViewPluginOptions = {
+  /**
+   * Provide a list of tracker namespaces to forward events to.
+   * By default, the events will be forwarded to the default mobile tracker.
+   */
+  trackerNamespaces?: string[];
+};
+
+/**
+ * Forwards events to Snowplow mobile trackers running in a WebView.
+ * @param configuration - Configuration. Specify certain tracker namespaces to forward events to.
+ */
+export function WebViewPlugin(configuration?: WebViewPluginOptions): BrowserPlugin {
+  let LOG: Logger;
+
+  return {
+    logger: (logger: Logger) => {
+      LOG = logger;
+    },
+
+    filter: (payload) => {
+      if (hasMobileInterface() === true) {
+        LOG.debug(`Payload (event ID: ${payload.eid}) was filtered out and forwarded to WebView tracker.`);
+
+        let atomicProperties = {
+          eventName: payload.e as string,
+          trackerVersion: payload.tv as string,
+          useragent: (payload.ua as string) ?? window.navigator.userAgent,
+          url: payload.url as string | undefined,
+          title: payload.page as string | undefined,
+          referrer: payload.refr as string | undefined,
+          category: payload.se_ca as string | undefined,
+          action: payload.se_ac as string | undefined,
+          label: payload.se_la as string | undefined,
+          property: payload.se_pr as string | undefined,
+          value: payload.se_va !== undefined ? parseFloat(payload.se_va as string) : undefined,
+          minXOffset: payload.pp_mix !== undefined ? parseInt(payload.pp_mix as string) : undefined,
+          maxXOffset: payload.pp_max !== undefined ? parseInt(payload.pp_max as string) : undefined,
+          minYOffset: payload.pp_miy !== undefined ? parseInt(payload.pp_miy as string) : undefined,
+          maxYOffset: payload.pp_may !== undefined ? parseInt(payload.pp_may as string) : undefined,
+        };
+        let event = getSelfDescribingEventData(payload);
+        let entities = getEntities(payload);
+        let trackers: Array<string> | undefined = configuration?.trackerNamespaces;
+
+        trackWebViewEvent({ properties: atomicProperties, event: event, context: entities }, trackers);
+
+        return false;
+      } else {
+        return true;
+      }
+    },
+  };
+}
+
+function getSelfDescribingEventData(payload: Payload): SelfDescribingEvent | undefined {
+  if (payload.ue_pr) {
+    return JSON.parse(payload.ue_pr as string).data;
+  } else if (payload.ue_px) {
+    let decoded = base64urldecode(payload.ue_px as string);
+    return JSON.parse(decoded).data;
+  }
+  return undefined;
+}
+
+function getEntities(payload: Payload): SelfDescribingJson[] {
+  if (payload.co) {
+    return JSON.parse(payload.co as string)['data'];
+  } else if (payload.cx) {
+    let decoded = base64urldecode(payload.cx as string);
+    return JSON.parse(decoded)['data'];
+  }
+  return [];
+}
---
+++ plugins/browser-plugin-webview/test/webview.test.ts
@@ -0,0 +1,202 @@
+import { addTracker, SharedState, EventStore, BrowserTracker } from '@snowplow/browser-tracker-core';
+import { newInMemoryEventStore, buildSelfDescribingEvent } from '@snowplow/tracker-core';
+import { WebViewPlugin } from '../src';
+import { hasMobileInterface, trackWebViewEvent } from '@snowplow/webview-tracker';
+
+jest.mock('@snowplow/webview-tracker');
+
+describe('WebView plugin', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  let idx = 1;
+  let eventStore: EventStore;
+  let tracker: BrowserTracker | null;
+
+  let mockHasMobileInterface = hasMobileInterface as jest.Mock<boolean>;
+  let mockTrackWebViewEvent = trackWebViewEvent as jest.Mock<void>;
+
+  it('Does not filter events if mobile interface not found', async () => {
+    mockHasMobileInterface.mockImplementation(() => {
+      return false;
+    });
+
+    eventStore = newInMemoryEventStore({});
+    const customFetch = async () => new Response(null, { status: 500 });
+    tracker = addTracker(`sp${idx++}`, `sp${idx++}`, 'js-4.0.0', '', new SharedState(), {
+      plugins: [WebViewPlugin()],
+      eventStore,
+      customFetch,
+    });
+
+    tracker?.trackPageView();
+
+    let events = await eventStore.getAllPayloads();
+    expect(events).toHaveLength(1);
+    expect(mockTrackWebViewEvent).not.toHaveBeenCalled();
+  });
+
+  it('Filters out the events if a mobile interface is present', async () => {
+    mockHasMobileInterface.mockImplementation(() => {
+      return true;
+    });
+
+    eventStore = newInMemoryEventStore({});
+    const customFetch = async () => new Response(null, { status: 500 });
+    tracker = addTracker(`sp${idx++}`, `sp${idx++}`, 'js-4.0.0', '', new SharedState(), {
+      plugins: [WebViewPlugin()],
+      eventStore,
+      customFetch,
+    });
+
+    tracker?.trackPageView();
+    tracker?.core.track(
+      buildSelfDescribingEvent({
+        event: {
+          schema: 'iglu:com.snowplowanalytics.snowplow.media/play_event/jsonschema/1-0-0',
+          data: {
+            a: 'b',
+          },
+        },
+      })
+    );
+
+    let events = await eventStore.getAllPayloads();
+    expect(events).toHaveLength(0);
+    expect(mockTrackWebViewEvent).toHaveBeenCalled();
+
+    let calls = mockTrackWebViewEvent.mock.calls;
+    // tracked two events
+    expect(calls).toHaveLength(2);
+
+    // page view event properties
+    expect(calls[0][0]).toMatchObject({
+      properties: {
+        eventName: 'pv',
+        trackerVersion: 'js-4.0.0',
+        useragent: expect.any(String),
+        url: expect.any(String),
+      },
+      context: [
+        {
+          schema: 'iglu:com.snowplowanalytics.snowplow/web_page/jsonschema/1-0-0',
+          data: {
+            id: expect.any(String),
+          },
+        },
+      ],
+    });
+
+    // self-describing event properties
+    expect(calls[1][0]).toMatchObject({
+      properties: {
+        eventName: 'ue',
+        trackerVersion: 'js-4.0.0',
+        useragent: expect.any(String),
+        url: expect.any(String),
+        title: undefined,
+        referrer: undefined,
+        category: undefined,
+        action: undefined,
+        label: undefined,
+        property: undefined,
+        value: undefined,
+        minXOffset: undefined,
+        maxXOffset: undefined,
+        minYOffset: undefined,
+        maxYOffset: undefined,
+      },
+      event: {
+        schema: 'iglu:com.snowplowanalytics.snowplow.media/play_event/jsonschema/1-0-0',
+        data: { a: 'b' },
+      },
+      context: [
+        {
+          schema: 'iglu:com.snowplowanalytics.snowplow/web_page/jsonschema/1-0-0',
+          data: {
+            id: expect.any(String),
+          },
+        },
+      ],
+    });
+
+    // no tracker namespaces provided
+    expect(calls[0][1]).toBeUndefined();
+  });
+
+  it('Decodes base64-encoded payloads', async () => {
+    mockHasMobileInterface.mockImplementation(() => {
+      return true;
+    });
+
+    eventStore = newInMemoryEventStore({});
+    const customFetch = async () => new Response(null, { status: 500 });
+    tracker = addTracker(`sp${idx++}`, `sp${idx++}`, 'js-4.0.0', '', new SharedState(), {
+      plugins: [WebViewPlugin()],
+      eventStore,
+      customFetch,
+    });
+
+    tracker?.core.setBase64Encoding(true);
+
+    tracker?.core.track(
+      buildSelfDescribingEvent({
+        event: {
+          schema: 'iglu:com.snowplowanalytics.snowplow/example/jsonschema/1-0-0',
+          data: {
+            hello: 'world',
+          },
+        },
+      })
+    );
+
+    let events = await eventStore.getAllPayloads();
+    expect(events).toHaveLength(0);
+    expect(mockTrackWebViewEvent).toHaveBeenCalled();
+
+    let calls = mockTrackWebViewEvent.mock.calls;
+    expect(calls).toHaveLength(1);
+
+    // decoded event properties
+    expect(calls[0][0]).toMatchObject({
+      properties: {
+        eventName: 'ue',
+        trackerVersion: 'js-4.0.0',
+        useragent: expect.any(String),
+        url: expect.any(String),
+      },
+      event: {
+        schema: 'iglu:com.snowplowanalytics.snowplow/example/jsonschema/1-0-0',
+        data: { hello: 'world' },
+      },
+      context: [
+        {
+          schema: 'iglu:com.snowplowanalytics.snowplow/web_page/jsonschema/1-0-0',
+          data: {
+            id: expect.any(String),
+          },
+        },
+      ],
+    });
+  });
+
+  it('Passes a configured list of tracker namespaces', async () => {
+    mockHasMobileInterface.mockImplementation(() => {
+      return true;
+    });
+
+    eventStore = newInMemoryEventStore({});
+    const customFetch = async () => new Response(null, { status: 500 });
+    tracker = addTracker(`sp${idx++}`, `sp${idx++}`, 'js-4.0.0', '', new SharedState(), {
+      plugins: [WebViewPlugin({ trackerNamespaces: ['sp1', 'sp2'] })],
+      eventStore,
+      customFetch,
+    });
+
+    tracker?.trackPageView();
+    let calls = mockTrackWebViewEvent.mock.calls;
+    expect(calls).toHaveLength(1);
+    expect(calls[0][1]).toEqual(['sp1', 'sp2']);
+  });
+});
